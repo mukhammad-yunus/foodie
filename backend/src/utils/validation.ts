bf11748b762e1eafdaa1310e51172a0d2d7ef476
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2026-02-12 11:38:34 +0100

    feat(backend): add validation helper

---
+++ backend/src/utils/validation.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+
+export function handleValidationErrors(req: Request, res: Response, next: NextFunction) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array().map(err => ({
+        param: err.type,
+        msg: err.msg
+      }))
+    });
+  }
+  next();
+}
